38e708031b7b039697753573f95b23726f8a0e2a
Nadine273 <tomns@mail.ru>
2024-06-13 06:57:40 +0200

    Transport is done

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,8 +1,17 @@
-public class Bicycle {
-    public String modelName;
-    public int wheelsCount;
+public class Bicycle extends Transport {
+
+    public Bicycle (String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
+    }
+    public void check(Bicycle bicycle){
+    if (bicycle != null) {
+        System.out.println("Обслуживаем велосипед" + bicycle.getModelName());
+        for (int i = 0; i < bicycle.getWheelsCount(); i++) {
+            bicycle.updateTyre();
+        }
     }
 }
